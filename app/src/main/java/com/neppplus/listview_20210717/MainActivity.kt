92bed35a3ec5cd4dd208ad97f0aba49610c43d89
조경진 <cho881020@gmail.com>
2021-07-17 14:50:46 +0900

    길게 누르면 -> 해당 학생 삭제 -> 어댑터에게도 알림

--- app/src/main/java/com/neppplus/listview_20210717/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20210717/MainActivity.kt
@@ -58,6 +58,12 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("길게눌린학생", clickedStudent.name)
 
+//            길게 눌린 학생을 목록에서 제거하자.
+            mStudentList.remove(clickedStudent)
+
+//            원본 목록 내용 변화 -> 어댑터변수에게도 알려줘야함.
+            mAdapter.notifyDataSetChanged()
+
             return@setOnItemLongClickListener true
 
         }
